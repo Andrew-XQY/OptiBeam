871c9118743d37e0ad9aae41ce2215056644b970
Xu <qiyuanxu@liverpool.ac.uk>
2024-03-10 16:31:08 +0000

    change import name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='OptiBeam',
+    name='optibeam',
     version='0.1.0',
     author='Andrew Xu',
     author_email='qiyuanxu95@gmail.com',
