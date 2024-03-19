80b512a2bc02fafdcd2871c4f3a7c5f03b80f747
Xu <qiyuanxu@liverpool.ac.uk>
2024-03-19 08:16:21 +0000

    version update

--- optibeam/__init__.py
+++ optibeam/__init__.py
@@ -1,1 +1,11 @@
 # This __init__.py is intentionally left empty to allow the package to be imported as a module.
+
+# __init__.py for optibeam package
+
+from . import utils
+from . import evaluation
+from . import visualization
+from . import training
+
+__all__ = ['utils', 'evaluation', 'visualization', 'training']
+
