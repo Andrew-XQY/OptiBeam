18a67a615a9f3e6e130b8b4d43a8a20ef0d9b171
Andrew-XQY <qiyuanxu95@gmail.com>
2024-04-04 15:36:54 +0000

    Bump version: 0.1.39 → 0.1.40

--- tests/sphinx/source/conf.py
+++ tests/sphinx/source/conf.py
@@ -77,7 +77,7 @@ except ImportError:
 
 project = "OptiBeam"
 author = optibeam.__author__
-release = "0.1.39"
+release = "0.1.40"
 copyright = f'2024, {author}'
 
 # -- General configuration ---------------------------------------------------
