c6f2b6594539bc951e100d4a4bbb4894e08561ce
Xu <qiyuanxu@liverpool.ac.uk>
2024-05-28 07:17:06 +0100

    modify

--- tests/unit_tests/test_dmd.py
+++ tests/unit_tests/test_dmd.py
@@ -10,12 +10,12 @@ DMD = dmd.ViALUXDMD(ALP4(version = '4.3'))
 for i in range(10000000):
     # Generate the mosaic image
     img = simulation.create_mosaic_image(size=1024)
-    M = simulation.compile_transformation_matrix(image=img, radians= i*np.pi/180) # radians= i*np.pi/180
+    M = simulation.compile_transformation_matrix(image=img) # radians= i*np.pi/180
     img = simulation.apply_transformation_matrix(img, M)
     img = simulation.pixel_value_remap(img, 255)
     # img = np.ones((1024, 1920), dtype=np.uint8)*255
     img = DMD.display_image(img)
-    time.sleep(1)
+    time.sleep(0.3)
     
 DMD.end()
 
--- tests/unit_tests/test_dmd_distribution.py
+++ tests/unit_tests/test_dmd_distribution.py
@@ -4,17 +4,18 @@ import time
 
 # Load the Vialux .dll
 DMD = dmd.ViALUXDMD(ALP4(version = '4.3'))
-canvas = simulation.DynamicPatterns(*(512, 512))
+dim = 128
+canvas = simulation.DynamicPatterns(*(dim, dim))
 canvas._distributions = [simulation.GaussianDistribution(canvas) for _ in range(10)]
 
 for i in range(1000000):
     canvas.update()
     img = canvas.get_image()
     img = simulation.pixel_value_remap(img)
-    img = simulation.macro_pixel(img, size=2)
+    img = simulation.macro_pixel(img, size=int(1024/dim))
     DMD.display_image(img)
     
-    #time.sleep(0.2)
+    #time.sleep(0.1)
     
 DMD.end()
 
